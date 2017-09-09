8d104627f45d9fd80577aa80ef231ad96ea08f4c
Kobie <prodigy2006@gmail.com>
2017-09-10 00:04:54 +0200

    Temp changes

--- VendingMachine/Program.cs
+++ VendingMachine/Program.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+
+using System;
 
 namespace VendingMachine
 {
@@ -10,6 +8,8 @@ namespace VendingMachine
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("*******************Vending Machine*********************");
+
         }
     }
 }
---
+++ VendingMachine/VendingOptions.cs
@@ -0,0 +1,27 @@
+﻿
+
+using NDesk.Options;
+
+namespace VendingMachine
+{
+    public class VendingOptions
+    {
+        public bool Showhelp { get; set; }
+        public bool AddProduct { get; set; }
+        public bool ListProducts { get; set; }
+        public bool BuyItem { get; set; }
+
+        public static OptionSet CreateOptionSet(VendingOptions arguments)
+        {
+            var options = new OptionSet()
+            {
+                {"help","Show help",  x => arguments.Showhelp = true },
+                {"AddProduct", "Adds new product", x => arguments.AddProduct = true },
+                {"ListProducts", "Shows available products", x => arguments.ListProducts = true },
+                {"BuyItem", "Buy Item from Vending Machine", x => arguments.BuyItem = true }
+            };
+
+            return options;
+        }
+    }
+}
